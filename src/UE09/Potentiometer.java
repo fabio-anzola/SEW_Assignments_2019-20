a756866b099e4e1d42631dfb5c3422e539a27cb9
Anzola Fabio <anzola.fabio@gmail.com>
2019-12-16 17:58:32 +0100

    Added Assignment #09e

---
+++ src/UE09/Potentiometer.java
@@ -0,0 +1,27 @@
+package UE09;
+
+/**
+ * Potentiometer class
+ *
+ * @author fabioanzola
+ */
+public class Potentiometer extends Resistor {
+    /**
+     * Constructor
+     *
+     * @param res double
+     */
+    public Potentiometer(double res) {
+        super(res);
+    }
+
+    /**
+     * Sets value
+     *
+     * @param val double
+     */
+    public void setValue(double val) {
+        super.setValue(val);
+    }
+}
+
